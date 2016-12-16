fadf7df9cc19397e09e0a0702aec40efea965a11
alisinabh <alisina.bm@gmail.com>
2016-12-16 17:47:20 +0330

    Simple structure of Jalali module

--- lib/jalali.ex
+++ lib/jalali.ex
@@ -1,2 +1,21 @@
 defmodule Jalali do
+  @moduledoc """
+  The Jalali calendar is a solar calendar that was used in Persia and Afganistan.
+  This module can convert gregorian datetimes to jalali datetimes and vice versa.
+  """
+
+  @doc """
+  Converts a jalali date to its equivalent in gregorian calendar
+  """
+  @spec to_gregorian(DateTime.t) :: DateTime.t
+  def to_gregorian(datetime) do
+
+  end
+
+  @doc """
+  Converts a gregorian date to its equivalent in jalali calendar
+  """
+  def to_jalali(datetime) do
+
+  end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Jalali.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :timex]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule Jalali.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:timex, "~> 3.0"}]
   end
 end
