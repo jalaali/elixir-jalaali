76b73d0dca32cc420485e55c71dca2a6e8dcaa6c
alisinabh <alisina.bm@gmail.com>
2016-12-17 21:21:32 +0330

    simple tests fixes

--- test/jalaali_test.exs
+++ test/jalaali_test.exs
@@ -7,13 +7,13 @@ defmodule JalaaliTest do
     assert 1 + 1 == 2
   end
 
-  test "convert regular jalali to gregorian" do
-    test_time = Timex.local
-    assert test_time == to_gregorian(to_jalali(test_time))
+  test "convert regular jalaali date to gregorian date" do
+    test_time = DateTime.utc_now
+    assert test_time == to_gregorian(to_jalaali(test_time))
   end
 
   test "method works with old dates" do
-    test_time = ~D[0621-03-26]
-    assert to_jalali(test_time) == ~D[0000-01-02]
+    test_time = ~D[2016-12-17]
+    assert to_jalaali(test_time) == ~D[1395-09-27]
   end
 end
