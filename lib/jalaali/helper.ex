8d02ad1c83afb1e0d9ee51e9a2af3aee3a0d3e2b
shahryarjb <shahryar.tbiz@gmail.com>
2021-09-20 21:47:59 +0430

    add functions to convert miladi to Jalaali

--- lib/jalaali/helper.ex
+++ lib/jalaali/helper.ex
@@ -1,7 +1,5 @@
 defmodule Jalaali.Helper do
 
-
-
   @type year :: neg_integer() | integer()
   @type month :: pos_integer
   @type day :: pos_integer
@@ -32,6 +30,7 @@ defmodule Jalaali.Helper do
   @callback day_of_year(year, month, day) :: 1..366
 
 
+  @spec offset_to_string(number, number, any, any) :: binary
   def offset_to_string(utc, std, zone, format \\ :extended)
   def offset_to_string(0, 0, "Etc/UTC", _format), do: "Z"
 
@@ -43,6 +42,7 @@ defmodule Jalaali.Helper do
     format_offset(total, hour, minute, format)
   end
 
+  @spec format_offset(any, integer, integer, :basic | :extended) :: binary
   def format_offset(total, hour, minute, :extended) do
     sign(total) <> zero_pad(hour, 2) <> ":" <> zero_pad(minute, 2)
   end
@@ -51,20 +51,60 @@ defmodule Jalaali.Helper do
     sign(total) <> zero_pad(hour, 2) <> zero_pad(minute, 2)
   end
 
+  @spec zone_to_string(any, any, any, binary) :: binary
   def zone_to_string(0, 0, _abbr, "Etc/UTC"), do: ""
   def zone_to_string(_, _, abbr, zone), do: " " <> abbr <> " " <> zone
 
   def sign(total) when total < 0, do: "-"
   def sign(_), do: "+"
 
+  @spec zero_pad(integer, non_neg_integer) :: binary
   def zero_pad(val, count) do
     num = Integer.to_string(val)
     :binary.copy("0", count - byte_size(num)) <> num
   end
 
+  @spec div_mod(integer, integer) :: {integer, integer}
   def div_mod(int1, int2) do
     div = div(int1, int2)
     mod = int1 - div * int2
     {div, mod}
+  end
+
+
+  # Helper functions to create a custom Jalaali time to input on HTML source
+  @spec miladi_to_jalaali(map()) :: binary
+  def miladi_to_jalaali(datetime) do
+    {:ok, jalaali_datetime} = DateTime.convert(datetime, Jalaali.Calendar)
+    jalaali_datetime
+    |> DateTime.to_string()
+    |> String.replace("Z", "")
+  end
+
+  @spec jalaali_create(map(), binary()) :: %{day_number: pos_integer, month_name: pos_integer, year_number: integer}
+  def jalaali_create(time_need, "number") do
+    {:ok, jalaali_date} = Date.convert(time_need, Jalaali.Calendar)
+    %{day_number: jalaali_date.day, month_name: jalaali_date.month, year_number: jalaali_date.year}
+  end
+
+  @spec jalaali_create(map()) :: %{day_number: pos_integer, month_name: String.t(), year_number: integer}
+  def jalaali_create(time_need) do
+    {:ok, jalaali_date} = Date.convert(time_need, Jalaali.Calendar)
+    %{day_number: jalaali_date.day, month_name: get_month(jalaali_date.month), year_number: jalaali_date.year}
+  end
+
+  @spec get_month(integer()) :: String.t()
+  def get_month(id) when id in 1..12 do
+    {_id, name} = [
+      {1, "فروردین"}, {2, "اردیبهشت"}, {3, "خرداد"}, {4, "تیر"}, {5, "مرداد"}, {6, "شهریور"},
+      {7, "مهر"}, {8, "آبان"}, {9, "آذر"}, {10, "دی"}, {11, "بهمن"}, {12, "اسفند"},
+    ]
+    |> Enum.find(fn {month_id, _month_persian_name} -> month_id == id end)
+    name
+  end
+
+  @spec fix_month_and_day(String.t()) :: binary
+  def fix_month_and_day(string_number) do
+    if String.length("#{string_number}") == 1, do: "0#{string_number}", else: "#{string_number}"
   end
 end
